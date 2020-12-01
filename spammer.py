47ac7b9ef70abe8a4a39b3d09f815483dad3ce9c
DiddlyXD <75302861+DiddlyXD@users.noreply.github.com>
2020-12-01 15:01:06 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,28 @@
+# this is a Stupid spammer bot made by DiddlyXD
+# This is just a bot that spammes in any chating service
+# first you have to download PyAutoGui and Kite because I recommend using kite
+# to do that you need to go to Terminal > type "Pip install pyautogui"
+# WARNING: You NEED to have PIP 20.3.3 or later or this will not work
+# this is the code:
+
+import pyautogui
+import time
+
+time.sleep(5)
+# now here you need to make a new text file
+# after creating paste one of the longest scripts in history
+# here i am going to use the bee movie script because it the one of the worst script in history
+f = open("script")
+for word in f:
+    # now here the pyautogui comes in place
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
+    # the pyautogui command makes the bot usable if you don't do install pyautogui and pip 20.3.3 or later then you have
+    # or you have to type the script on your own LOL and you don't want that so install does 2 things
+    # now when you run the command you have 5 seconds to switch to any chatting service
+    # you need to click on the chat area where you will enter messages
+    # it will start in 5 seconds
+    # and you need to keep your cursor in the chat area and don't move it
+    # if you want to stop spamming then just move your cursor to anyone of the corners on you screen
+    # Now you can enjoy spamming
+    # fun fact : this will ruin friendships more than UNO, so  be carefull
